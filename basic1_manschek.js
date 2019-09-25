3702171a3c716d59d783e748171ade5c58e766ff
calamanshi <calamanshi@gmx.at>
2019-09-25 10:21:46 +0200

    hi

---
+++ basic1_manschek.js
@@ -0,0 +1,23 @@
+// function calcAge(Birthday){
+
+// 	var date1 = new Date();
+// 	var year = date1.getFullYear();
+// 	var age1 = year - Birthday;
+// 	var age2 = age1 - 1;
+
+// 	console.log (age1, age2);
+
+// }
+// calcAge(1992);
+// document.write ("sack");
+
+var argu1 = "I am a hero of Katzenpolster"
+var argu2 = 11;
+
+function kuerzer (a,b){
+	c = a.substring(0, b);
+}
+kuerzer(argu1, argu2)
+document.write(c);
+
+document.write ("<br>")

5201cad72c3897d31e58a38adbc2b4805c318118
calamanshi <calamanshi@gmx.at>
2019-09-25 15:51:22 +0200

    challenge added

--- challenge.js
+++ challenge.js
@@ -1,3 +1,5 @@
+function wuerfl () {
+
 document.getElementsByClassName("p1z1")[0].innerHTML=Math.floor(Math.random()*6.9999);
 document.getElementsByClassName("p1z2")[0].innerHTML=Math.floor(Math.random()*6.9999);
 document.getElementsByClassName("p1z3")[0].innerHTML=Math.floor(Math.random()*6.9999);
@@ -8,27 +10,23 @@ document.getElementsByClassName("p2z3")[0].innerHTML=Math.floor(Math.random()*6.
 document.getElementsByClassName("p1erg")[0].innerHTML= Number(document.getElementsByClassName("p1z1")[0].innerHTML)+Number(document.getElementsByClassName("p1z2")[0].innerHTML)+Number(document.getElementsByClassName("p1z3")[0].innerHTML);
 document.getElementsByClassName("p2erg")[0].innerHTML= Number(document.getElementsByClassName("p2z1")[0].innerHTML)+Number(document.getElementsByClassName("p2z2")[0].innerHTML)+Number(document.getElementsByClassName("p2z3")[0].innerHTML);
 
-
 if
 (Number(document.getElementsByClassName("p1erg")[0].innerHTML) > Number(document.getElementsByClassName("p2erg")[0].innerHTML))
 {
 document.getElementsByClassName("ausruf")[0].innerHTML="Hubsi has won."
+document.body.style.backgroundColor = "#ADD8E6";
+}
+else if (Number(document.getElementsByClassName("p1erg")[0].innerHTML) == Number(document.getElementsByClassName("p2erg")[0].innerHTML))
+{
+document.getElementsByClassName("ausruf")[0].innerHTML="It's a tie!"
+document.body.style.backgroundColor = "#fff";
 }
 else 
 {
-document.getElementsByClassName("ausruf")[0].innerHTML="Baerbl has won."
+	document.getElementsByClassName("ausruf")[0].innerHTML="Baerbel has won."
+document.body.style.backgroundColor = "pink";
 }
-
-
-
-
-
-// if
-// (Number(document.getElementsByClassName("p1erg")[0].innerHTML) > Number(document.getElementsByClassName("p2erg")[0].innerHTML))
-// {
-// document.getElementsByClassName("ausruf")[0].innerHTML="Hubsi has won."
-// }
-// else 
-// {
-// document.getElementsByClassName("ausruf")[0].innerHTML="Baerbl has won."
+}
+// switch(){
+// 	case "p1won": 
 // }

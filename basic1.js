bbe97f856b5dc5a21d89c2be19bf3a8d5402f9b8
Christian Simic <christian.simicibwl@hotmail.com>
2019-09-25 10:28:00 +0200

    coomit

--- basic1.js
+++ basic1.js
@@ -1,11 +1,25 @@
-function calcAge(Birthday){
+// function calcAge(Birthday){
 
-	var date1 = new Date();
-	var year = date1.getFullYear();
-	var age1 = year - birthday;
-	var age2 = age1 - 1;
+// 	var date1 = new Date();
+// 	var year = date1.getFullYear();
+// 	var age1 = year - irthday;
+// 	var age2 = age1 - 1;
 
-	console.log
+// 	console.log(age1,age2)
 
-}
-calcAge(27);
+// }
+// calcAge(27);
+
+
+
+		function personalInformation(name,surname,age,shortDescription){
+
+			var sentence = "Hi I am " + name + " " + surname + " I am " + age + " years old and I am a " + shortDescription;
+
+
+			return sentence;
+
+
+		}
+
+		personalInformation("John","Doe",25,"future programmer");
